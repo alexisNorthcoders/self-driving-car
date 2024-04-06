0c203c7f870bd45524e8c09d0b964e6397b46618
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-06 01:01:13 +0100

    add dashes to the road lines

--- road.js
+++ road.js
@@ -7,23 +7,29 @@ class Road {
     this.left = x - width / 2;
     this.right = x + width / 2;
 
-    const infinity = 1000000000;
+    const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
   }
 
-  draw(ctx){
-    ctx.lineWidth=5
-    ctx.strokeStyle="white"
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
 
-    ctx.beginPath()
-    ctx.moveTo(this.left,this.top)
-    ctx.lineTo(this.left,this.bottom)
-    ctx.stroke()
-
-    ctx.beginPath()
-    ctx.moveTo(this.right,this.top)
-    ctx.lineTo(this.right,this.bottom)
-    ctx.stroke()
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      if (i > 0 && i < this.laneCount) {
+        
+        ctx.setLineDash([20,20]);
+        
+      } else {
+        ctx.setLineDash([]);
+      }
+     
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
   }
 }
