91cf94721c16ebd05e44aa1d9632ea39f809bf16
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-08 15:40:28 +0100

    add pause/continue functionality, add neural network visualizer animations, add function to generate many AI cars

--- main.js
+++ main.js
@@ -1,37 +1,74 @@
-const carCanvas = document.getElementById("carCanvas")
-carCanvas.width=200
-const networkCanvas = document.getElementById("networkCanvas")
-networkCanvas.width=300
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
 
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+const N = 100;
+const cars = generateCars(N);
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")];
 
-const carCtx = carCanvas.getContext("2d")
-const networkCtx = networkCanvas.getContext("2d")
-const road = new Road(carCanvas.width/2,carCanvas.width*0.9)
-const car= new Car(road.getLaneCenter(1),100,30,50,"AI",5)
-const traffic =[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY")
-]
+let isPause = false;
+function stopAnimation() {
+  cancelAnimationFrame(animationFrameId);
+  drawPause();
+}
+function drawPause() {
+    isPause=true
+  const canvas = document.getElementById("carCanvas");
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 300, canvas.width, 100);
+  ctx.fillStyle = "rgba(0,0,0,1)";
+  ctx.font = "48px sans-serif";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("PAUSE", canvas.width / 2, canvas.height / 2);
+}
+document.addEventListener("keydown", (event) => {
+  if (event.key === "s") {
+    stopAnimation();
+  } else if (event.key === "a" && isPause) {
+    animate();
+    isPause=false
+  }
+});
+animate();
 
-animate()
+function generateCars(N) {
+  const cars = [];
+  for (let i = 1; i <= N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 5));
+  }
+  return cars;
+}
 
-function animate(){
-    for (let i=0;i<traffic.length;i++){
-        traffic[i].update(road.borders,[])
-    }
-    car.update(road.borders,traffic)
-    carCanvas.height=window.innerHeight
-    networkCanvas.height=window.innerHeight
-    carCtx.save()
-    carCtx.translate(0,-car.y+carCanvas.height*0.7)
-    road.draw(carCtx)
-    
-    for (let i=0;i<traffic.length;i++){
-        traffic[i].draw(carCtx,"red")
-    }
-    car.draw(carCtx,"blue")
+function animate(time) {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic);
+  }
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
+  carCtx.save();
+  carCtx.translate(0, -cars[0].y + carCanvas.height * 0.7);
+  road.draw(carCtx);
 
-    carCtx.restore()
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(carCtx, "red");
+  }
+  carCtx.globalAlpha = 0.2;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
 
-    Visualizer.drawNetwork(networkCtx,car.brain)
-    requestAnimationFrame(animate)
+  carCtx.restore();
+
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, cars[0].brain);
+  animationFrameId = requestAnimationFrame(animate);
 }
