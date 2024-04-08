0d94be87d9a9dea1dcdc9218145e1a93c6e69a9c
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-08 17:14:59 +0100

    add more traffic and train the bestBrain

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
     new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY"),
     new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY"),
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY"),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY"),
+    new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY"),
+    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY"),
 ];
 
 let isPause = false;
