8ed418c94884902ab571a0724e2f080af3ce5790
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-08 16:02:54 +0100

    add buttons to save and delete localstorage bestBrain

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0]
+if (localStorage.getItem("bestBrain")){
+    bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"))
+}
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")];
 
 let isPause = false;
@@ -56,7 +60,7 @@ function animate(time) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].update(road.borders, traffic);
   }
-  const bestCar = cars.find(
+  bestCar = cars.find(
     car => car.y === Math.min( ...cars.map(car => car.y))
   )
   carCanvas.height = window.innerHeight;
